9bb630ccb58ff9f3c9c8dbd5a9b41c8de8838fc6
jeodom17 <jeodom17@gmail.com>
2021-10-27 23:19:38 -0400

    added files in seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -4,3 +4,16 @@ const { User, Post, Comments } = require('../models');
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentsData = require('./commentsData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
