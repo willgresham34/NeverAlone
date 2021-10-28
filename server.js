9f1b8be0655b8500f8b0bb52c3d8904f1ad95af9
Will Gresham <willgresham34@gmail.com>
2021-10-28 13:43:29 -0400

    nodemailer start

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require("path");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
-const nodemail = require("nodemailer");
+const nodemailer = require("nodemailer");
 const routes = require("./controllers");
 const Sequelize = require("sequelize");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config");
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
@@ -33,7 +33,6 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
-
 
 app.use(routes);
 
