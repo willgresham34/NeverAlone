59de2619d85e3df0ff63e0b612083d7e8e5872e6
jeodom17 <jeodom17@gmail.com>
2021-10-30 15:18:17 -0400

    link js/comments, change comment btn color

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,25 +1,23 @@
+const handleComments = async function (event) {
+  event.preventDefault();
 
-const handleComments = async function(event) {
-    event.preventDefault();
-  
-   
-    const body = document.querySelector('textarea[name="userComment"]').value;
-  
-    if (body) {
-      await fetch('/api/comments', {
-        method: 'POST',
-        body: JSON.stringify({
-          body
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      document.location.reload();
-    }
-  };
-  
-  document
-    .querySelector('#new-comment')
-    .addEventListener('submit', handleComments);
+  const body = document.querySelector('textarea[name="userComment"]').value;
+
+  if (body) {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        body,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    document.location.reload();
+  }
+};
+
+document
+  .querySelector("#new-comment-btn")
+  .addEventListener("submit", handleComments);
