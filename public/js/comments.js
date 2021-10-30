510a1ebba765cb61858c4038062d5e837fa77f87
jeodom17 <jeodom17@gmail.com>
2021-10-29 20:14:38 -0400

    add comments.js in public

---
+++ public/js/comments.js
@@ -0,0 +1,25 @@
+
+const handleComments = async function(event) {
+    event.preventDefault();
+  
+   
+    const body = document.querySelector('textarea[name="userComment"]').value;
+  
+    if (body) {
+      await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          body
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      document.location.reload();
+    }
+  };
+  
+  document
+    .querySelector('#new-comment')
+    .addEventListener('submit', handleComments);
