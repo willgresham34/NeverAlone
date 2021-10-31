3318d945947ef25b6a23bebdfa7748fabfd506db
tlaze <tomlazore@gmail.com>
2021-10-31 17:18:09 -0400

    Posts now update

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -1,6 +1,6 @@
 const router = require ('express').Router();
 const{ Post, User } = require('../../models');
-// const {withAuth} = require('../../utils/auth');
+const {withAuth} = require('../../utils/auth');
 
 
 // Creates new post
@@ -18,5 +18,30 @@ router.post('/homepage', async (req,res) => {
         res.status(500).json(err);
     }
 });
+
+
+// Changes the value of selected post in the dashboard
+router.put('/profile/edit/:id', withAuth, async (req,res) => {
+    try{
+        const editPost = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if(editPost){
+            res.status(200).end();
+        }
+        else{
+            res.status(404).end();
+        }
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+
 
 module.exports = router;
