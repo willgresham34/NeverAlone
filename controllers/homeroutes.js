6f42324cef5e88893e2217951a861cd9d25eaddc
Will Gresham <willgresham34@gmail.com>
2021-10-31 15:18:42 -0400

    trying to fix

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -26,7 +26,18 @@ router.get("/sign-up", async (req, res) => {
       res.redirect("/homepage");
     }
     res.render("signup", { logged_in: req.session.loggedIn });
-    // console.log("Data:", req.body.email);
+    // const userEmail = await Post.findAll({
+    //   where: {
+    //     user_id: req.session.userId,
+    //   },
+    //   include: [
+    //     {
+    //       model: User.email,
+    //     },
+    //   ],
+    // });
+
+    console.log("Data:", userEmail);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -48,37 +59,24 @@ router.get("/login", async (req, res) => {
 // get profile page when logged in
 
 router.get("/profile", withAuth, async (req, res) => {
-
   try {
     const postData = await Post.findAll({
       where: {
         user_id: req.session.userId,
       },
-<<<<<<< HEAD
-    });
-    console.log("bio", userData.bio);
-    const dataUser = userData.get({ plain: true });
-    res.render("profile", { loggedIn: req.session.loggedIn, dataUser });
-  } catch (err) {
-=======
       include: [
         {
-          model: User
-        }
-      ]
-    })
+          model: User,
+        },
+      ],
+    });
 
-    const userPosts = postData.map(post => post.get({ plain: true }));
+    const userPosts = postData.map((post) => post.get({ plain: true }));
 
     console.log("Posts on Profile (userPosts): ", userPosts);
 
-    res.render('profile', { userPosts, loggedIn: req.session.loggedIn });
-  } 
-
-
-
-  catch (err) {
->>>>>>> 6c55aaf4aaaf74da9f6f51ddc0c87e0b97529abf
+    res.render("profile", { userPosts, loggedIn: req.session.loggedIn });
+  } catch (err) {
     res.status(500).json(err);
   }
 });
@@ -88,28 +86,20 @@ router.get("/profile", withAuth, async (req, res) => {
 router.get("/homepage", withAuth, async (req, res) => {
   try {
     const postData = await Post.findAll({
-<<<<<<< HEAD
-      include: [{ model: User }, { model: Comments }],
-=======
-      include:[ User,
-      {
-        model:Comments,
-        include: User,
-      },
-    ],
->>>>>>> 6c55aaf4aaaf74da9f6f51ddc0c87e0b97529abf
+      include: [
+        User,
+        {
+          model: Comments,
+          include: User,
+        },
+      ],
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
     let randomIndex = Math.floor(Math.random() * quoteList.length);
     let randomQuote = quoteList[randomIndex];
-<<<<<<< HEAD
 
-    console.log("Posts", posts);
-=======
-    
     console.log("Posts on Homepage: ", posts);
->>>>>>> 6c55aaf4aaaf74da9f6f51ddc0c87e0b97529abf
 
     res.render("homepage", {
       posts,
