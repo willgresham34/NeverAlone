de9c4883467ce915cb8e43aecf4a385b14f11e72
Will Gresham <willgresham34@gmail.com>
2021-11-01 00:12:09 -0400

    comment file renamed

--- models/index.js
+++ models/index.js
@@ -1,28 +1,27 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comments = require('./Comments');
+const User = require("./User");
+const Post = require("./Post");
+const Comments = require("./Comments");
 
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Comments.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-  Comments.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
-  });
-
+Comments.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
-    User,
-    Post,
-    Comments
+  User,
+  Post,
+  Comments,
 };
