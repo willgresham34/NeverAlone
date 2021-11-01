60d36d1080b09dd275cd6ef69d519a6bae91e0d2
tlaze <tomlazore@gmail.com>
2021-10-31 21:20:27 -0400

    lets users know correct length of password when signing up

--- models/User.js
+++ models/User.js
@@ -36,14 +36,14 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [1],
       },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [6],
       },
     },
     bio: {
